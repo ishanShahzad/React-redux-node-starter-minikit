552ff0b7e742546eedbb79d247fc66cd124c2733
ishan shahzad <3006761319ishan@gmail.com>
2019-09-29 23:04:35 +0500

    css folder structure changed

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import reducers from "./reducers";
+import App from "./components/App";
 
-import store from './store';
-import App from './components/App';
-
-require('./styles.scss');
+require("./css/styles.scss");
+const enhancer = compose(applyMiddleware(thunk));
+const store = createStore(reducers, composeWithDevTools(enhancer));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('app')
+  document.getElementById("app")
 );
